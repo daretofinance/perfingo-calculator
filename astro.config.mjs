2d926aba58c0963a1f5f15988a00de1f05fe39d8
darren <darrenljw96@gmail.com>
2024-07-12 19:40:34 +0800

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 import icon from "astro-icon";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()]
+  integrations: [tailwind(), 
+                react(), 
+                icon(),
+                partytown({
+                  config: {
+                    forward: ['dataLayer.push'],
+                  }
+                })
+              ]
 });
